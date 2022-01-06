45b7a2dd3ff0f2f94f0e4a6d71759476da8bb6a8
Ranjana Yadav <79187120+ranjana5696@users.noreply.github.com>
2022-01-06 12:42:36 +0530

    Update second.ts

--- second.ts
+++ second.ts
@@ -3,16 +3,16 @@ function add(n1: number, n2: number) {
         return n1 + n2;
 }
 
-function printResult(num: number) {
-    console.log('Result: ' + num);
-}
+// function printResult(num: number) {
+//     console.log('Result: ' + num);
+// }
 
 // function printResult(num: number): undefined {
 //     console.log('Result: ' + num);
 //     return;
-// }
+// // }
 
-printResult(add(5, 87));
+// printResult(add(5, 87));
 
 let combineValues: (a: number, b: number) => number;
 
